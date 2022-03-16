6d7cc640bbe7bcc221eecc3bbbf44d17a68e597f
Jdickson96 <51116993+Jdickson96@users.noreply.github.com>
2022-03-16 11:52:08 +0000

    Sort Test Update

--- JMJ_SortManagerApp/SortingTests/SortTest.cs
+++ JMJ_SortManagerApp/SortingTests/SortTest.cs
@@ -1,17 +1,15 @@
 using NUnit.Framework;
 using SortManager;
 using System;
+using System.Collections.Generic;
 
-namespace SortingTests;
+namespace SortTests;
 
-public class SortTest
+public class BubbleSortTest
 {
 
-//------------------------------BUBBLE SORT-------------------------------------------------------------
-
-    [Category("Bubble Sort")]
-    [Description("Empty Bubble Array Test")]
-    [TestCase(new int[] { }, new int[] {  })]
+    [Category("Empty Array Test")]
+    [TestCase(new int[] { }, new int[] { })]
 
     public void GivenEmptyArray_Bubble_ReturnEmptyArray(int[] input, int[] expected)
     {
@@ -19,9 +17,8 @@ public class SortTest
         Assert.That(expected, Is.EqualTo(result));
     }
 
-    [Category("Bubble Sort")]
-    [Description("Single Digit Bubble Array Test")]
-    [TestCase(new int[] {2}, new int[] {2})]
+    [Category("Single Digit Array Test")]
+    [TestCase(new int[] { 2 }, new int[] { 2 })]
 
     public void GivenSingleDigitArray_Bubble_ReturnSingleDigitArray(int[] input, int[] expected)
     {
@@ -29,11 +26,10 @@ public class SortTest
         Assert.That(expected, Is.EqualTo(result));
     }
 
-    [Category("Bubble Sort")]
-    [Description("0-10 Bubble Array Test")]
+    [Category("0-10 Simple Array Test")]
     [TestCase(new int[] { 10, 7, 8, 4, 2, 6, 9, 3, 5, 1 }, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })]
 
-    public void GivenOnetoTenIntArray_Bubble_ReturnCorrectOrderArray(int[] input, int[] expected)
+    public void GivenTenIntArray_Bubble_ReturnCorrectOrderArray(int[] input, int[] expected)
     {
         int[] result = BubbleSort.BubbleArraySort(input);
         Assert.That(expected, Is.EqualTo(result));
@@ -42,11 +38,11 @@ public class SortTest
     [Category("Bubble Sort")]
     [Description("1000 Bubble Array Test")]
 
-    public void GivenOnetoTenIntArray_Bubble_ReturnCorrectOrderArray()
+    public void GivenThousandIntArray_Bubble_ReturnCorrectOrderArray()
     {
         int[] input = new int[1000];
         Random rand = new Random(1);
-        for (int i = 0; i<1000; i++)
+        for (int i = 0; i < 1000; i++)
         {
             input[i] = rand.Next(0, 1001);
         }
@@ -54,11 +50,10 @@ public class SortTest
         Array.Sort(input);
         Assert.That(result, Is.EqualTo(input));
     }
-
-//------------------------------MERGE SORT-------------------------------------------------------------
-
-[Category("Merge Sort")]
-    [Description("Empty Merge Array Test")]
+}
+public class MergeSortTest
+{
+    [Category("Empty Array Test")]
     [TestCase(new int[] { }, new int[] { })]
 
     public void GivenEmptyArray_Merge_ReturnEmptyArray(int[] input, int[] expected)
@@ -67,9 +62,8 @@ public class SortTest
         Assert.That(expected, Is.EqualTo(result));
     }
 
-    [Category("Merge Sort")]
-    [Description("Single Digit Merge Array Test")]
-    [TestCase(new int[] {2}, new int[] {2})]
+    [Category("Single Digit Array Test")]
+    [TestCase(new int[] { 2 }, new int[] { 2 })]
 
     public void GivenSingleDigitArray_Merge_ReturnSingleDigitArray(int[] input, int[] expected)
     {
@@ -77,20 +71,19 @@ public class SortTest
         Assert.That(expected, Is.EqualTo(result));
     }
 
-    [Category("Merge Sort")]
-    [Description("0-10 Merge Array Test")]
+    [Category("0-10 Simple Array Test")]
     [TestCase(new int[] { 10, 7, 8, 4, 2, 6, 9, 3, 5, 1 }, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })]
 
-    public void GivenOnetoTenIntArray_Merge_ReturnCorrectOrderArray(int[] input, int[] expected)
+    public void GivenTenIntArray_Merge_ReturnCorrectOrderArray(int[] input, int[] expected)
     {
         int[] result = MergeSort.MergeArraySort(input);
         Assert.That(expected, Is.EqualTo(result));
     }
 
-    //------------------------------SLOW MERGE SORT--------------------------------------------------------
-
-    [Category("Old Merge Sort")]
-    [Description("Empty Old Merge Array Test")]
+}
+public class OldMergeSortTest
+{
+    [Category("Empty Array Test")]
     [TestCase(new int[] {2}, new int[] {2})]
 
     public void GivenEmptyArray_OldMerge_ReturnEmptyArray(int[] input, int[] expected)
@@ -99,8 +92,7 @@ public class SortTest
         Assert.That(expected, Is.EqualTo(result));
     }
 
-    [Category("Merge Sort")]
-    [Description("Single Digit Old Merge Array Test")]
+    [Category("Single Digit Array Test")]
     [TestCase(new int[] { }, new int[] { })]
 
     public void GivenSingleDigitArray_OldMerge_ReturnSingleDigitArray(int[] input, int[] expected)
@@ -109,11 +101,10 @@ public class SortTest
         Assert.That(expected, Is.EqualTo(result));
     }
 
-    [Category("Merge Sort")]
-    [Description("0-10 Old Merge Array Test")]
+    [Category("0-10 Simple Array Test")]
     [TestCase(new int[] { 10, 7, 8, 4, 2, 6, 9, 3, 5, 1 }, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })]
 
-    public void GivenOnetoTenIntArray_OldMerge_ReturnCorrectOrderArray(int[] input, int[] expected)
+    public void GivenTenIntArray_OldMerge_ReturnCorrectOrderArray(int[] input, int[] expected)
     {
         int[] result = MergeSort.MergeArraySortOld(input);
         Assert.That(expected, Is.EqualTo(result));
